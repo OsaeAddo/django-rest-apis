d10f267cf369805c5585f9fde81c60625ecf9c40
OsaeAddo <immanueloaddo@gmail.com>
2022-04-20 20:39:07 +0000

    add views for api app

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from yaml import serialize
+from rest_framework import generics
 
-# Create your views here.
+from books.models import Book
+
+from todos.models import Todo
+
+from .serializers import BookSerializer
+
+# Book API
+class BookAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
+# Todo API
