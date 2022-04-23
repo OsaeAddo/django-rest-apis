42e72763c5767a18b56cc3fe40b70167e6d9c0cc
OsaeAddo <immanueloaddo@gmail.com>
2022-04-23 10:03:48 +0000

    update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from .views import BookAPIView, DetailTodoView, ListTodoView
 
 urlpatterns = [
+    #Book API
     path('book/', BookAPIView.as_view()),
+
+    # Todo API
     path('todo/', ListTodoView.as_view()),
     path('todo/<int:pk>/', DetailTodoView.as_view()),
 ]
