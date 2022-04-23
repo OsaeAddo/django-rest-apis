a6618c14a560692157ea98aef2b1ced35c7ba249
OsaeAddo <immanueloaddo@gmail.com>
2022-04-23 10:00:51 +0000

    add and render the mock data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const list = [
+  {
+    "id": 1,
+    "title": "Study for Physics exam",
+    "body": "prepare for upcoming physics exam"
+  },
+  {
+      "id": 2,
+      "title": "Call prospect",
+      "body": "make a business call"
+  },
+  {
+      "id": 3,
+      "title": "Visit my bby girl",
+      "body": "surprise my girl with a visit"
+  }
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { list };
+  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        {this.state.list.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
