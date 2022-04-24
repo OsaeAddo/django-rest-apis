aba069a0b9c8d3c9e0e80c1d51b2c9b4839a4303
OsaeAddo <immanueloaddo@gmail.com>
2022-04-24 15:52:03 +0000

    add tests and update models

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # For Blog
-class Posts(models.Model):
+class Post(models.Model):
     """
     A model for creating a blog
     """
