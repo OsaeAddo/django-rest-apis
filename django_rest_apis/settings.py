5ae689c7ccda491b7ee1511fd36aaf7f72a06948
OsaeAddo <immanueloaddo@gmail.com>
2022-05-21 16:20:59 +0000

    add session and token authentication

--- django_rest_apis/settings.py
+++ django_rest_apis/settings.py
@@ -62,7 +62,11 @@ if DEBUG:
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ],
 }
 
 MIDDLEWARE = [
