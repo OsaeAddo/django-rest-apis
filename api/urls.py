51910af80ccfc4e8b23da9f0bad4c586bf03946e
OsaeAddo <immanueloaddo@gmail.com>
2022-05-21 17:39:26 +0000

    refactor urls to use ViewSets & Routers

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,32 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import BookAPIView, DetailTodoView, ListTodoView, PostListView, PostDetailView
 
-urlpatterns = [
-    #Book API
-    path('book/', BookAPIView.as_view()),
+# from .views import BookAPIView, DetailTodoView, ListTodoView, PostListView, PostDetailView, UserDetail, UserList
 
-    # Todo API
-    path('todo/', ListTodoView.as_view()),
-    path('todo/<int:pk>/', DetailTodoView.as_view()),
+# Using Routers and Viewsets
+from .views import UserViewSet, PostViewSet
 
-    #Posts API
-    path('post/v1/', PostListView.as_view()), # v1 represents the API version, a good idea
-    path('post/v1/<int:pk>/', PostDetailView.as_view()),
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('posts', PostViewSet, basename='posts')
+
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     # Users API
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>', UserDetail.as_view()),
+    
+#     #Book API
+#     path('book/', BookAPIView.as_view()),
+
+#     # Todo API
+#     path('todo/', ListTodoView.as_view()),
+#     path('todo/<int:pk>/', DetailTodoView.as_view()),
+
+#     #Posts API
+#     path('post/v1/', PostListView.as_view()), # v1 represents the API version, a good idea
+#     path('post/v1/<int:pk>/', PostDetailView.as_view()),
+# ]
