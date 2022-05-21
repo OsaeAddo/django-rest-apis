e7cf6691532f0b54a96f0e31b4e01b0f7985f6eb
OsaeAddo <immanueloaddo@gmail.com>
2022-05-21 17:39:47 +0000

    refactor the project urls

--- django_rest_apis/urls.py
+++ django_rest_apis/urls.py
@@ -19,8 +19,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
+    
     path('api-auth/', include('rest_framework.urls')),
+    
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
