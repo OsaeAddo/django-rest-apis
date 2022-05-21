beea9d4e20b9de74bd48c450cfb09e16284cc32f
OsaeAddo <immanueloaddo@gmail.com>
2022-05-21 16:24:47 +0000

    load dj-rest-auth in endpoints to project urls

--- django_rest_apis/urls.py
+++ django_rest_apis/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', include('books.urls')),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
