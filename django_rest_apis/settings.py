14760858d1e93511565c4d0d5d893f54a7346c51
OsaeAddo <immanueloaddo@gmail.com>
2022-05-22 17:37:54 +0000

    add default schema class

--- django_rest_apis/settings.py
+++ django_rest_apis/settings.py
@@ -67,6 +67,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 MIDDLEWARE = [
