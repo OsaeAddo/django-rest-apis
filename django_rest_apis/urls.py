69ae8613f801ae7a83421269b335c92454fd5164
OsaeAddo <immanueloaddo@gmail.com>
2022-05-22 17:41:23 +0000

    add imports for api docs and schema

--- django_rest_apis/urls.py
+++ django_rest_apis/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
+
+API_TITLE = "Blog, Books, Todos API"
+API_DESCRIPTION = "A web API for creating and editing blog posts, books and todos"
+schema_view = get_schema_view(title=API_TITLE)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
